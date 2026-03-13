e21f466d5ef0c5886683a3ec880c1612f9b22dc4
A-SHI-M <1647dasashim1647@gmail.com>
2026-03-13 18:26:41 +0600

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,4 @@
-import logging
-import os
+import logging, os
 from datetime import datetime
 
 
@@ -13,6 +12,3 @@ logging.basicConfig(
     format = "[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
 )
-
-if __name__=="__main__":
-    logging.info("logging has started")
